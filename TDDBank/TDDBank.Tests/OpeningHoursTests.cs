8e3ddfc6413b266a484d310fad13c115d0735dc5
André <ruland@outlook.de>
2021-06-17 16:10:25 +0200

    OPening Hours Lab

---
+++ TDDBank/TDDBank.Tests/OpeningHoursTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDDBank.Tests
+{
+    [TestClass]
+    public class OpeningHoursTests
+    {
+        [TestMethod]
+        [DataRow(2021, 6, 14, 10, 30, true)]//mo
+        [DataRow(2021, 6, 14, 10, 29, false)]//mo
+        [DataRow(2021, 6, 14, 10, 0, false)] //mo
+        [DataRow(2021, 6, 14, 18, 59, true)] //mo
+        [DataRow(2021, 6, 14, 19, 00, false)] //mo
+        [DataRow(2021, 6, 19, 13, 0, true)] //sa
+        [DataRow(2021, 6, 19, 16, 0, false)] //sa
+        [DataRow(2021, 6, 20, 20, 0, false)] //so
+        public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
+        {
+            var dt = new DateTime(y, M, d, h, m, 0);
+            var oh = new OpeningHours();
+
+            Assert.AreEqual(result, oh.IsOpen(dt));
+        }
+    }
+}
